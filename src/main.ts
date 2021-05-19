63659444a3b8703b0e73e6b13569cd8cdcc1a381
a11thwn <althwncc@gmail.com>
2021-05-20 00:35:56 +0800

    完成 workflow

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { Request, Response } from "express"
+import { Request, Response } from 'express';
 const app = express();
 const port = 3000;
 
@@ -20,18 +20,18 @@ const data = [
   {
     id: 1,
     title: '关山月',
-    content: '明月出天山，苍茫云海间'
+    content: '明月出天山，苍茫云海间',
   },
   {
     id: 2,
     title: '望岳',
-    content: '会当凌绝顶，一览众山小'
+    content: '会当凌绝顶，一览众山小',
   },
   {
     id: 3,
     title: '忆江南',
-    content: '日出江花红胜火，春来江水绿如蓝'
-  }
+    content: '日出江花红胜火，春来江水绿如蓝',
+  },
 ];
 
 app.get('/posts', (resquest: Request, response: Response) => {
@@ -43,7 +43,7 @@ app.get('/posts/:postId', (resquest: Request, response: Response) => {
   const { postId } = resquest.params;
 
   // 查找具体内容
-  const posts = data.filter(item => item.id == parseInt(postId, 10));
+  const posts = data.filter((item) => item.id == parseInt(postId, 10));
 
   // 作出响应
   response.send(posts[0]);
@@ -66,6 +66,6 @@ app.post('/posts', (resquest: Request, response: Response) => {
   response.header({ 'sing-along': 'how i wander where you are' });
   // 作出响应
   response.send({
-    message: `成功创建了内容： ${content}`
+    message: `成功创建了内容： ${content}`,
   });
 });

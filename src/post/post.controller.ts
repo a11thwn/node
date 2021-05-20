33299bb57b78d2b685a73ddc3e31da1a7422191d
a11thwn <althwncc@gmail.com>
2021-05-20 21:50:37 +0800

    学习数据处理-储存内容前保存一次

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -4,7 +4,7 @@ import { getPosts } from './post.service';
 /**
  *   内容列表
  */
-export const index = (
+export const index = async (
   request: Request,
   response: Response,
   next: NextFunction,
@@ -12,6 +12,6 @@ export const index = (
   if (request.headers.authorization !== 'SECRET') {
     return next(new Error());
   }
-  const posts = getPosts();
+  const posts = await getPosts();
   response.send(posts);
 };

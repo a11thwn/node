579aa0d497c423abd2b0235508883344e3fe058c
a11thwn <althwncc@gmail.com>
2021-05-20 20:13:01 +0800

    完成一章

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import postRouter from '../post/post.router';
+import { defaultErrorHandler } from './app.middleware';
 
 /**
  *  创建应用
@@ -9,7 +10,17 @@ const app = express();
 /**
  *  处理 JSON
  */
-app.use(express.json(), postRouter);
+app.use(express.json());
+
+/**
+ *  路由
+ */
+app.use(postRouter);
+
+/**
+ *  默认异常处理器
+ */
+app.use(defaultErrorHandler);
 
 /**
  *  导出应用
---
+++ src/post/post.service.ts
@@ -0,0 +1,17 @@
+/**
+ *  获取内容列表
+ */
+export const getPosts = () => {
+  const data = [
+    {
+      content: '明月出天山，苍茫云海间',
+    },
+    {
+      content: '明月出天山，苍茫云海间1',
+    },
+    {
+      content: '明月出天山，苍茫云海间2',
+    },
+  ];
+  return data;
+};

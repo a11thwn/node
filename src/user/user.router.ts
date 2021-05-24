a64099f794eeeb3675afac410ff7237cd98d0d89
a11thwn <althwncc@gmail.com>
2021-05-24 19:49:53 +0800

    创建用户接口

--- src/user/user.router.ts
+++ src/user/user.router.ts
@@ -1,8 +1,12 @@
 import express from 'express';
 import * as userController from './user.controller';
 
-const router = exports.Router();
+const router = express.Router();
 
+/**
+ *  创建用户
+ */
+router.post('/users', userController.store);
 /**
  *  导出路由
  */

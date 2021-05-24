d31dc7000b036e1461c72f3bdf1faa0851224ec8
a11thwn <althwncc@gmail.com>
2021-05-24 21:10:33 +0800

    完成注册用户

--- src/user/user.router.ts
+++ src/user/user.router.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import * as userController from './user.controller';
+import { hashPassword, validateUserData } from './user.middleware';
 
 const router = express.Router();
 
 /**
  *  创建用户
  */
-router.post('/users', userController.store);
+router.post('/users', validateUserData, hashPassword, userController.store);
 /**
  *  导出路由
  */

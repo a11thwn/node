2d6859af4e26f2e14defd750a11125cb32dc808d
a11thwn <althwncc@gmail.com>
2021-05-26 21:56:43 +0800

    发现错误

---
+++ src/file/file.controller.ts
@@ -0,0 +1,40 @@
+import { Request, Response, NextFunction } from 'express';
+import _ from 'lodash';
+import { createFile } from './file.service';
+
+/**
+ * 上传文件
+ */
+export const store = async (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) => {
+  //当前用户
+  const { id: userId } = request.user;
+
+  //所属内容
+  const { post: postId } = request.query;
+
+  //文件信息
+  const fileInfo = _.pick(request.file, [
+    'orignalname',
+    'mimetype',
+    'filename',
+    'size',
+  ]);
+
+  try {
+    //保存文件信息
+    const data = await createFile({
+      ...fileInfo,
+      userId,
+      postId: parseInt(`${postId}`, 10),
+    });
+
+    //作出响应
+    response.status(201).send(data);
+  } catch (error) {
+    next(error);
+  }
+};

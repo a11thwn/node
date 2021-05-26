f4199b6e7de8559086ee3a02d6a7c99df3a3ced3
a11thwn <althwncc@gmail.com>
2021-05-26 18:50:54 +0800

    访问控制中间件

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import jwt from 'jsonwebtoken';
 import { PRIVATE_KEY } from '../app/app.config';
+import { connection } from '../app/database/mysql';
 
 /**
  *  签发信息
@@ -17,4 +18,30 @@ export const signToken = (options: SignTokenOptions) => {
 
   //提供数据
   return token;
+};
+
+/**
+ *  检查用户是否拥有制定资源
+ */
+interface PossessOptions {
+  resourceId: number;
+  resourceType: string;
+  userId: number;
+}
+export const possess = async (options: PossessOptions) => {
+  //准备选项
+  const { resourceId, resourceType, userId } = options;
+
+  // 准备查询
+  const statement = `
+  SELECT COUNT(${resourceType}.id) as cout
+  FROM ${resourceType}
+  WHERE ${resourceType}.id = ? AND userId = ?
+  `;
+
+  //检查拥有权
+  const [data] = await connection
+    .promise()
+    .query(statement, [resourceId, userId]);
+  return data[0].count ? true : false;
 };

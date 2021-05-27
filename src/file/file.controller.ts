0323c3b04b7c4c00a7f4c3352e42b804c6c5a734
a11thwn <althwncc@gmail.com>
2021-05-27 20:08:37 +0800

    完成处理文件

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -1,3 +1,5 @@
+import path from 'path';
+import fs from 'fs';
 import { Request, Response, NextFunction } from 'express';
 import _ from 'lodash';
 import { createFile, findFileById } from './file.service';
@@ -30,6 +32,7 @@ export const store = async (
       ...fileInfo,
       userId,
       postId: parseInt(`${postId}`, 10),
+      ...request.fileMetaData,
     });
 
     //作出响应
@@ -53,8 +56,35 @@ export const serve = async (
   try {
     //查找文件信息
     const file = await findFileById(parseInt(fileId, 10));
-    console.log(file.filename);
-    console.log(file.mimetype);
+    //要提供的文件尺寸
+    const { size } = request.query;
+
+    //文件名与目录
+    let filename = file.filename;
+    let root = 'uploads';
+    let resized = 'resized';
+
+    if (size) {
+      //可用的图像尺寸
+      const imageSizes = ['large', 'medium', 'thumbnail'];
+
+      //检查文件尺寸是否可用
+      if (!imageSizes.some((item) => item == size)) {
+        throw new Error('FILE_NOT_FOUND');
+      }
+    }
+
+    //检查文件是否存在
+    const fileExist = fs.existsSync(
+      path.join(root, resized, `${filename}-${size}`),
+    );
+
+    //设置文件名与目录
+    if (fileExist) {
+      filename = `${filename}-${size}`;
+      root = path.join(root, resized);
+    }
+
     //作出响应
     response.sendFile(file.filename, {
       root: 'uploads',
@@ -62,6 +92,29 @@ export const serve = async (
         'Content-Type': file.mimetype,
       },
     });
+  } catch (error) {
+    next(error);
+  }
+};
+
+/**
+ *  文件信息
+ */
+export const metadata = async (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) => {
+  //文件 ID
+  const { fileId } = request.params;
+  try {
+    //查询文件数据
+    const file = await findFileById(parseInt(fileId, 10));
+
+    //准备响应数据
+    const data = _.pick(file, ['id', 'size', 'width', 'height', 'metadata']);
+    //作出响应
+    response.send(data);
   } catch (error) {
     next(error);
   }

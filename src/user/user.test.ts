af896ffb61cfb45647c359a5f4caed609c86851a
a11thwn <althwncc@gmail.com>
2021-06-15 22:59:05 +0800

    完成《自动测试》

--- src/user/user.test.ts
+++ src/user/user.test.ts
@@ -5,6 +5,7 @@ import { connection } from '../app/database/mysql';
 import { signToken } from '../auth/auth.service';
 import { deleteUser, getUserById } from './user.service';
 import { UserModel } from './user.model';
+import exp from 'constants';
 
 /**
  *  准备测试
@@ -71,5 +72,80 @@ describe('测试创建用户接口', () => {
 
     // 作出断言
     expect(response.status).toBe(201);
+  });
+});
+
+/**
+ *  用户账户
+ */
+describe('测试用户账户接口', () => {
+  test('响应里应该包含制定的属性', async () => {
+    //请求接口
+    const response = await request(app).get(`/users/${testUserCreated.id}`);
+
+    //作出断言
+    expect(response.status).toBe(200);
+    expect(response.body.name).toBe(testUser.name);
+    expect(response.body).toMatchObject({
+      id: expect.any(Number),
+      name: expect.any(String),
+      avatar: null,
+    });
+  });
+
+  test('当用户不存在时，响应状态码是 404', async () => {
+    // 请求接口
+    const response = await request(app).get('/users/-1');
+
+    //作出断言
+    expect(response.status).toBe(404);
+  });
+});
+
+/**
+ *  更新用户
+ */
+describe('测试更新用户接口', () => {
+  test('更新用户时需要验证用户身份', async () => {
+    //请求接口
+    const response = await request(app).patch('/users');
+
+    //作出断言
+    expect(response.status).toBe(401);
+  });
+
+  test('更新用户数据', async () => {
+    //签发令牌
+    const token = signToken({
+      payload: { id: testUserCreated.id, name: testUserCreated.name },
+    });
+
+    //请求接口
+    const response = await request(app)
+      .patch('/users')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        validate: {
+          password: testUser.password,
+        },
+        update: {
+          name: testUserUpdated.name,
+          password: testUserUpdated.password,
+        },
+      });
+
+    //调取用户
+    const user = await getUserById(testUserCreated.id, { password: true });
+
+    // 对比密码
+    const matched = await bcrypt.compare(
+      testUserUpdated.password,
+      user.password,
+    );
+
+    //作出断言
+    expect(response.status).toBe(200);
+    expect(matched).toBeTruthy();
+    expect(user.name).toBe(testUserUpdated.name);
   });
 });
